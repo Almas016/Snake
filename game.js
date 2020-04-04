a0dfc754347193ac4c09ab70a34a07319d26a60e
ALMAS <namazbaev2000@gmail.com>
2020-04-04 07:01:12 +0600

    game js function direction

--- game.js
+++ game.js
@@ -15,3 +15,24 @@ let food = {
 	x: Math.floor((Math.random() * 17 + 1)) * box,
 	y: Math.floor((Math.random() * 15 + 3)) * box,
 };
+
+let snake = [];
+snake[0] = {
+	x: 9 * box,
+	y: 10 * box
+};
+
+document.addEventListener("keydown", direction);
+
+let dir;
+
+function direction(event) {
+	if(event.keyCode == 37 && dir != "right")
+		dir = "left";
+	else if(event.keyCode == 38 && dir != "down")
+		dir = "up";
+	else if(event.keyCode == 39 && dir != "left")
+		dir = "right";
+	else if(event.keyCode == 40 && dir != "up")
+		dir = "down";
+}
